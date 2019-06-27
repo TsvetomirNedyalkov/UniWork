bb9123c0a725320c46009e08861342bdaca76be3
TsvetomirNedyalkov <52162561+TsvetomirNedyalkov@users.noreply.github.com>
2019-06-27 16:11:56 +0300

    Add files via upload

---
+++ SnakeActivity.java
@@ -0,0 +1,398 @@
+package com.example.snake;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import java.io.IOException;
+import java.util.Random;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+
+public class SnakeActivity extends Activity {
+
+    SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the SnakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
+
+        // Make snakeEngine the view of the Activity
+        setContentView(snakeEngine);
+    }
+
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
+
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
+    }
+
+}
+
+class SnakeEngine extends SurfaceView implements Runnable {
+
+
+
+    // Game thread for the main game loop
+    private Thread thread = null;
+
+    // To hold a reference to the Activity
+    private Context context;
+
+    // For playing sound effects
+    private SoundPool soundPool;
+    private int snake_eat = -1;
+    private int snake_crash = -1;
+
+    // For tracking movement Heading
+    public enum Heading {UP, RIGHT, DOWN, LEFT}
+    // Start by heading to the right
+    private Heading heading = Heading.RIGHT;
+
+    // To hold the screen size in pixels
+    private int screenX;
+    private int screenY;
+
+    // How long is the snake
+    private int snakeLength;
+
+    // Position of the fruit.
+    private int fruitX;
+    private int fruitY;
+
+    // The size in pixels of a snake segment
+    private int blockSize;
+
+    // The size in segments of the playable area
+    private final int NUM_BLOCKS_WIDE = 40;
+    private int numBlocksHigh;
+
+    // Control pausing between updates
+    private long nextFrameTime;
+    // Update the game 10 times per second
+    private final long FPS = 10;
+    // There are 1000 milliseconds in a second
+    private final long MILLIS_PER_SECOND = 1000;
+// We will draw the frame much more often
+
+    // How many points does the player have
+    private int score;
+
+    // The location in the grid of all the segments
+    private int[] snakeXs;
+    private int[] snakeYs;
+
+    // Everything we need for drawing
+    // Is the game currently playing?
+    private volatile boolean isPlaying;
+
+    // A canvas for our paint
+    private Canvas canvas;
+
+    // Required to use canvas
+    private SurfaceHolder surfaceHolder;
+
+    // Some paint for our canvas
+    private Paint paint;
+
+    public SnakeEngine(Context context, Point size) {
+        super(context);
+
+        context = context;
+
+        screenX = size.x;
+        screenY = size.y;
+
+        // Work out how many pixels each block is
+        blockSize = screenX / NUM_BLOCKS_WIDE;
+        // How many blocks of the same size will fit into the height
+        numBlocksHigh = screenY / blockSize;
+
+        // Set the sound up
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        try {
+            // Create objects of the 2 required classes
+            // Use m_Context because this is a reference to the Activity
+            AssetManager assetManager = context.getAssets();
+            AssetFileDescriptor descriptor;
+
+            // Prepare the two sounds in memory
+            descriptor = assetManager.openFd("snake_eat.ogg");
+            snake_eat = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("snake_crash.ogg");
+            snake_crash = soundPool.load(descriptor, 0);
+
+        } catch (IOException e) {
+            // Error
+        }
+
+
+        // Initialize the drawing objects
+        surfaceHolder = getHolder();
+        paint = new Paint();
+
+        // If you score 200 you are rewarded with a crash achievement!
+        snakeXs = new int[200];
+        snakeYs = new int[200];
+
+        // Start the game
+        newGame();
+    }
+
+    @Override
+    public void run() {
+
+        while (isPlaying) {
+
+            // Update 10 times a second
+            if(updateRequired()) {
+                update();
+                draw();
+            }
+
+        }
+    }
+
+    public void pause() {
+        isPlaying = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+    }
+
+    public void resume() {
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void newGame() {
+        // Start with a single snake segment
+        snakeLength = 1;
+        snakeXs[0] = NUM_BLOCKS_WIDE / 2;
+        snakeYs[0] = numBlocksHigh / 2;
+
+        // Spawns the fruit
+        spawnFruit();
+
+        // Reset the score
+        score = 0;
+
+        // Setup nextFrameTime so an update is triggered
+        nextFrameTime = System.currentTimeMillis();
+    }
+
+    public void spawnFruit() {
+        Random random = new Random();
+        fruitX = random.nextInt(NUM_BLOCKS_WIDE - 1) + 1;
+        fruitY = random.nextInt(numBlocksHigh - 1) + 1;
+    }
+
+    private void eatFruit(){
+        // Increase the size of the snake
+        snakeLength++;
+        //replace fruit
+
+        spawnFruit();
+        //add to the score
+        score = score + 1;
+        soundPool.play(snake_eat, 1, 1, 0, 0, 1);
+    }
+
+    private void moveSnake(){
+        // Move the body
+        for (int i = snakeLength; i > 0; i--) {
+            // Start at the back and move it
+            // to the position of the segment in front of it
+            snakeXs[i] = snakeXs[i - 1];
+            snakeYs[i] = snakeYs[i - 1];
+
+            // Exclude the head because
+            // the head has nothing in front of it
+        }
+
+        // Move the head in the appropriate heading
+        switch (heading) {
+            case UP:
+                snakeYs[0]--;
+                break;
+
+            case RIGHT:
+                snakeXs[0]++;
+                break;
+
+            case DOWN:
+                snakeYs[0]++;
+                break;
+
+            case LEFT:
+                snakeXs[0]--;
+                break;
+        }
+    }
+
+    private boolean detectDeath(){
+        // Has the snake died?
+        boolean dead = false;
+
+        // Hit the screen edge
+        if (snakeXs[0] == -1) dead = true;
+        if (snakeXs[0] >= NUM_BLOCKS_WIDE) dead = true;
+        if (snakeYs[0] == -1) dead = true;
+        if (snakeYs[0] == numBlocksHigh) dead = true;
+
+        // Eaten itself?
+        for (int i = snakeLength - 1; i > 0; i--) {
+            if ((i > 4) && (snakeXs[0] == snakeXs[i]) && (snakeYs[0] == snakeYs[i])) {
+                dead = true;
+            }
+        }
+
+        return dead;
+    }
+
+    public void update() {
+        // Did the head of the snake eat Fruit?
+        if (snakeXs[0] == fruitX && snakeYs[0] == fruitY) {
+            eatFruit();
+        }
+
+        moveSnake();
+
+        if (detectDeath()) {
+            //start again
+            soundPool.play(snake_crash, 1, 1, 0, 0, 1);
+
+            newGame();
+        }
+    }
+
+    public void draw() {
+        // Get a lock on the canvas
+        if (surfaceHolder.getSurface().isValid()) {
+            canvas = surfaceHolder.lockCanvas();
+
+            // Fill the screen with Game Code School blue
+            canvas.drawColor(Color.argb(255, 26, 128, 182));
+
+            // Set the color of the paint to draw the snake white
+            paint.setColor(Color.argb(255, 255, 255, 255));
+
+            // Scale the HUD text
+            paint.setTextSize(90);
+            canvas.drawText("Score:" + score, 10, 70, paint);
+
+            // Draw the snake one block at a time
+            for (int i = 0; i < snakeLength; i++) {
+                canvas.drawRect(snakeXs[i] * blockSize,
+                        (snakeYs[i] * blockSize),
+                        (snakeXs[i] * blockSize) + blockSize,
+                        (snakeYs[i] * blockSize) + blockSize,
+                        paint);
+            }
+
+            // Set the color of the paint to draw fruit red
+            paint.setColor(Color.argb(255, 255, 0, 0));
+
+            // Draw fruit
+            canvas.drawRect(fruitX * blockSize,
+                    (fruitY * blockSize),
+                    (fruitX * blockSize) + blockSize,
+                    (fruitY * blockSize) + blockSize,
+                    paint);
+
+            // Unlock the canvas and reveal the graphics for this frame
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public boolean updateRequired() {
+
+        // Are we due to update the frame
+        if(nextFrameTime <= System.currentTimeMillis()){
+            // Tenth of a second has passed
+
+            // Setup when the next update will be triggered
+            nextFrameTime =System.currentTimeMillis() + MILLIS_PER_SECOND / FPS;
+
+            // Return true so that the update and draw
+            // functions are executed
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                if (motionEvent.getX() >= screenX / 2) {
+                    switch(heading){
+                        case UP:
+                            heading = Heading.RIGHT;
+                            break;
+                        case RIGHT:
+                            heading = Heading.DOWN;
+                            break;
+                        case DOWN:
+                            heading = Heading.LEFT;
+                            break;
+                        case LEFT:
+                            heading = Heading.UP;
+                            break;
+                    }
+                } else {
+                    switch(heading){
+                        case UP:
+                            heading = Heading.LEFT;
+                            break;
+                        case LEFT:
+                            heading = Heading.DOWN;
+                            break;
+                        case DOWN:
+                            heading = Heading.RIGHT;
+                            break;
+                        case RIGHT:
+                            heading = Heading.UP;
+                            break;
+                    }
+                }
+        }
+        return true;
+    }
+
+}
+
